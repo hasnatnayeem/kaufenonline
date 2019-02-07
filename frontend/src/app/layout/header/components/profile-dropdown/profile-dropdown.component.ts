72a957c666c33dd3f143d39504364a9b41a9f779
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2019-02-07 15:32:26 +0100

    Fix logout

--- frontend/src/app/layout/header/components/profile-dropdown/profile-dropdown.component.ts
+++ frontend/src/app/layout/header/components/profile-dropdown/profile-dropdown.component.ts
@@ -44,8 +44,9 @@ export class ProfileDropdownComponent implements OnInit, OnChanges, OnDestroy {
 
   logout() {
     this.subnav = !this.subnav;
-    // this.subList$.push(this.authService.logout().
-    //   subscribe(_ => this.router.navigate(['auth', 'login'])));
+    // this.subList$.push(this.authService.logout());
+    this.authService.logout();
+    this.router.navigate(['auth', 'login']);
   }
 
   login() {
